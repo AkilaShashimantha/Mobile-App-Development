43380e1811b95b81cc28fa6cea2b688d4ceb44cc
Akila Shashimantha <akilashashimantha84@gmail.com>
2025-01-28 14:36:41 +0530

    Create Sign Up page and Re design Home Page

--- app/src/main/java/com/example/elawalu/SignUp.java
+++ app/src/main/java/com/example/elawalu/SignUp.java
@@ -1,6 +1,9 @@
 package com.example.elawalu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SignUp extends AppCompatActivity {
 
+    private Button signUpBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +22,17 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         getSupportActionBar().hide();
+
+        Button signUpBtn = findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUp.this,Login.class);
+                startActivity(intent);
+                
+
+            }
+        });
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
